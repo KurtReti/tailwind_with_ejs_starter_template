8812789231155d6d1240700a3a99a4186adbbbd3
KurtReti <kurt.reti@gmail.com>
2024-04-17 15:55:22 +1000

    Setting up project and defining model for task. Connected MongoDB and defined test route for creating documents.

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
+//includes
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require('mongoose')
+const Task = require('./models/Task')
+
+// setting up express with const
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// DB URI String
+const DB_URI = 'mongodb+srv://kreti:Vaerus44@atlascluster.hsr8mne.mongodb.net/TodoList?retryWrites=true&w=majority&appName=AtlasCluster'
+
+mongoose.connect(DB_URI)
+	.then((result) => {
+    app.listen(PORT, () => {
+      console.log(`The app started successfully on http://localhost:${PORT}`);
+      console.log('DB connected successfully')
+    });
+  })
+	.catch((err) => console.log(err))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// design file
+// setting ejs as view engine
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// morgan logging each request as it comes in
+app.use(morgan("dev"));
+
 // routers
 app.get("/", (req, res) => {
-  res.render("index");
+ ;const tasks = [
+		{title: 'blah blah 1', done_status: true},
+		{title: 'blah 2', done_status: false},
+		]
+	res.render('index', {tasks: tasks})
+});
+
+app.get("/create", (req, res) => {
+  res.render("create");
+});
+
+app.get('/add-task', (req, res) => {
+	const task = new Task({
+		title: 'Task 1',
+		done_status: false
+	})
+
+	task.save()
+		.then((result) => {
+			res.send(result)
+		})
+})
+
+// 404 route
+app.use((req, res) => {
+  res.status(404).render("404");
 });
 
 // server listening
-app.listen(PORT, () => {
-  console.log(`The app start on http://localhost:${PORT}`);
-});
+
